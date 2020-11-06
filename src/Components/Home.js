80df0e9f068543729912bc9b424209605e809953
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-07 00:39:18 +0500

    update home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+       <h1 className="background">wellcome To shoes Collection</h1>
+       <img src="https://image.shutterstock.com/image-vector/store-shoes-men-2d-illustration-600w-1404903071.jpg" className="bg" />
+    </div>
+  );
+}
+
+export default Home;
